8b0c1aa9b1a863b74ca4970b3807c34f196ac22c
lironh <lironenk@gmail.com>
2019-11-01 11:33:10 +0200

    refactor: wip - fade

--- projects/ng-micro-interact/src/lib/ng-micro-interact-factory.ts
+++ projects/ng-micro-interact/src/lib/ng-micro-interact-factory.ts
@@ -2,11 +2,13 @@ import {ElementRef} from '@angular/core';
 import {Blink} from './interactions/blink';
 import {Bounce} from './interactions/bounce';
 import {defaultDuration} from './consts';
-import {InteractionType} from './ng-micro-interact.types';
+import { InteractionType } from './ng-micro-interact.types';
+import {Fade} from './interactions/fade';
 
 const INTERACTION_TYPES: InteractionType = {
     blink: Blink,
-    bounce: Bounce
+    bounce: Bounce,
+    fade: Fade,
 };
 
 export class NgMicroInteractFactory {
