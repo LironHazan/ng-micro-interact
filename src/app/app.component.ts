e74383457a6b8ac61552878c4ccb2236ff710aaa
lironh <lironenk@gmail.com>
2019-11-02 15:56:53 +0200

    refactor(tweaked demo):

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  types = [
+    'blink',
+    'bounce',
+    'fade',
+    'flicker',
+    'groove',
+    'jelly',
+    'jerk',
+    'pop',
+    'shake',
+    'squeeze',
+    'swing',
+    'tada'
+  ];
 }
